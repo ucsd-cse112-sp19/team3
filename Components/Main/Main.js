8ef01bb8c406d8c34a907c4ba837fa1866ba4e52
Zihan Yi <z4yi@ucsd.edu>
2019-04-22 20:15:44 -0700

    init

--- Components/Main/Main.js
+++ Components/Main/Main.js
@@ -1,2 +1,22 @@
 const testLog = 'test log.';
 console.log(testLog);
+
+
+window.customElements.define('x-foo-with-markup', class extends HTMLElement {
+/** This is a description of the foo function. */
+  connectedCallback() {
+    this.innerHTML = '<b>A Custom Element!</b>';
+  }
+
+  /** This is a description of the foo function. */
+  constructor() {
+    // If you define a constructor, always call super() first!
+    // This is specific to CE and required by the spec.
+    super();
+
+    // Setup a click listener on <app-drawer> itself.
+    this.addEventListener('click', (e) => {
+      alert('clicked!');
+    });
+  }
+});
