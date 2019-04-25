ecaf89184320b606186e96ea29fcf5ef56be728d
Alex Z <alexz1012@yahoo.com>
2019-04-25 01:19:37 -0700

    feat: added component internationalization

--- Components/CoreHello/CoreHello.js
+++ Components/CoreHello/CoreHello.js
@@ -5,27 +5,25 @@ class CoreHello extends HTMLElement {
   /** Constructor of the class */
   constructor() {
     super();
-    
+
     // Create shadow DOM and attach template content
     const shadowRoot = this.attachShadow({mode: 'open'});
     CoreHello.template = document.createElement('template');
     const templateContent = CoreHello.template.content;
-    
+
     // Create greeting
-    var greeting = "Hello World";
-    var lang = this.getAttribute("lang");
-    if(lang === "jp") {
-      greeting = "Kon'nichiwa sekai";
-    }
-    else if(lang === "es") {
-      greeting = "Hola Mundo";
-    }
-    else if(lang === "fr") {
-      greeting = "Bonjour le monde";
+    let greeting = 'Hello World';
+    const lang = this.getAttribute('lang');
+    if (lang === 'jp') {
+      greeting = 'Kon\'nichiwa sekai';
+    } else if (lang === 'es') {
+      greeting = 'Hola Mundo';
+    } else if (lang === 'fr') {
+      greeting = 'Bonjour le monde';
     }
     CoreHello.template.innerHTML = greeting + ` <slot></slot>`;
-    
-    
+
+
     shadowRoot.appendChild(templateContent.cloneNode(true));
   }
 
