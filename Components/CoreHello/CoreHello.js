08b5268a1b06fc8d4da40b6e0cf367f1cb681d99
Alexander Zhou <alz030@ucsd.edu>
2019-04-29 16:30:24 -0700

    feat: keep component css self-contained

--- Components/CoreHello/CoreHello.js
+++ Components/CoreHello/CoreHello.js
@@ -22,15 +22,79 @@ class CoreHello extends HTMLElement {
       greeting = 'Bonjour le monde';
     }
 
+    // Import CSS
+    const importStyle = `<style>.greeting {
+                  font-size: 100px;
+                  font-family: 'Franklin Gothic Medium',
+                  'Arial Narrow', Arial, sans-serif;
+                }
+
+                .rainbow {
+                  animation: rainbow 2.5s linear;
+                  animation-iteration-count: infinite;
+                }
+
+                @keyframes rainbow {
+                  100%,
+                  0% {
+                color: rgb(255, 0, 0);
+                  }
+
+                  8% {
+                color: rgb(255, 127, 0);
+                  }
+
+                  16% {
+                color: rgb(255, 255, 0);
+                  }
+
+                  25% {
+                color: rgb(127, 255, 0);
+                  }
+
+                  33% {
+                color: rgb(0, 255, 0);
+                  }
+
+                  41% {
+                color: rgb(0, 255, 127);
+                  }
+
+                  50% {
+                color: rgb(0, 255, 255);
+                  }
+
+                  58% {
+                color: rgb(0, 127, 255);
+                  }
+
+                  66% {
+                color: rgb(0, 0, 255);
+                  }
+
+                  75% {
+                color: rgb(127, 0, 255);
+                  }
+
+                  83% {
+                color: rgb(255, 0, 255);
+                  }
+
+                  91% {
+                color: rgb(255, 0, 127);
+                  }
+                }</style>`;
+
     // Set CSS
-    const isRainbow = this.getAttribute('rainbow') === '';
+    const isRainbow = this.hasAttribute('rainbow');
     let styling = `<div class="greeting">`;
     if (isRainbow) {
       styling = `<div class="rainbow greeting">`;
     }
 
-    CoreHello.template.innerHTML = `<style>@import 'CoreHello.css'</style>` +
+    CoreHello.template.innerHTML = importStyle +
     styling + greeting + ` <slot></slot>`;
+
 
     shadowRoot.appendChild(templateContent.cloneNode(true));
   }
