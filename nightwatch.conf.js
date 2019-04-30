e063e3a6b619bfb2bc4400b8aa215847083115f5
rixlai <rixktl@gmail.com>
2019-04-29 18:30:49 -0700

    feat: add browserstack with basic test

---
+++ nightwatch.conf.js
@@ -0,0 +1,28 @@
+const nightwatch_config = {
+  src_folders: ['NightWatch.tests'],
+  selenium : {
+    "start_process" : false,
+    "host" : "hub-cloud.browserstack.com",
+    "port" : 80
+  },
+
+  test_settings: {
+    default: {
+      desiredCapabilities: {
+        'browserstack.user': 'rixlai1',
+        'browserstack.key': process.env.BROWSERSTACK_KEY,
+        'browserstack.local': true,
+        'browser': 'chrome'
+      }
+    }
+  }
+};
+
+// Code to copy seleniumhost/port into test settings
+for(let i in nightwatch_config.test_settings){
+  const config = nightwatch_config.test_settings[i];
+  config['selenium_host'] = nightwatch_config.selenium.host;
+  config['selenium_port'] = nightwatch_config.selenium.port;
+}
+
+module.exports = nightwatch_config;
