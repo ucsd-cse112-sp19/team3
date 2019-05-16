1defc7ce3a378ac00e0aab8fbc8cc51664d210b7
Alexz1008 <32420795+Alexz1008@users.noreply.github.com>
2019-05-16 16:04:07 -0700

    feat: slider component (#26)
    * feat: set up initial slider webcomponent
    
    * feat: initialize slider webcomponent
    
    * feat: updated slider attributes

--- Components/Slider/Slider.js
+++ Components/Slider/Slider.js
@@ -1,77 +1,106 @@
 'use strict';
 
-/** Class for Core Hello component */
-class Slider extends HTMLElement {
+/** Class for Custom Slider component */
+class CustomSlider extends HTMLElement {
   /** Constructor of the class */
   constructor() {
     super();
 
     // Create shadow DOM and attach template content
     const shadowRoot = this.attachShadow({mode: 'open'});
-    Slider.template = document.createElement('slider_template');
-    const templateContent = Slider.template.content;
+    CustomSlider.template = document.createElement('template');
+    const templateContent = CustomSlider.template.content;
 
     // Import CSS
     const importStyle = `<style>
-        .slidecontainer {
-        width: 100%;
-        }
-        
-        .slider {
-        -webkit-appearance: none;
-        width: 100%;
-        height: 10px;
-        border-radius: 5px;
-        background: #d3d3d3;
-        outline: none;
-        opacity: 0.7;
-        -webkit-transition: .2s;
-        transition: opacity .2s;
-        }
-        
-        .slider:hover {
-        opacity: 1;
-        }
-        
-        .slider::-webkit-slider-thumb {
-        -webkit-appearance: none;
-        appearance: none;
-        width: 23px;
-        height: 24px;
-        border: 0;
-        background: url('contrasticon.png');
-        cursor: pointer;
-        }
-        
-        .slider::-moz-range-thumb {
-        width: 23px;
-        height: 24px;
-        border: 0;
-        background: url('contrasticon.png');
-        cursor: pointer;
-        }
-        </style>`;
+      .medium-input {
+        width: 20px;
+        height: 20px;
+      }
+      .medium-input {
+        width: 50px;
+        height: 50px;
+      }
+      .large-input {
+        width: 100px;
+        height: 100px;
+      }
+    </style>`;
 
-    // Set CSS
+    // Attributes
+    let value = 0;
+    let min = 0;
+    let max = 100;
+    let disabled = '';
+    let vertical = '';
+    let show_input = '';
+    let input_class = 'small-input';	
 
-    Slider.template.innerHTML = importStyle +
-    `<div class="slidecontainer">`+ ` <slot></slot>`;
+    // Store the value of the attribute value
+    if (this.hasAttribute('value')) {
+      value = this.getAttribute('value');
+    }
+    // Store the value of the attribute min
+    if (this.hasAttribute('min')) {
+      min = this.getAttribute('min');
+    }
+    // store the value of the attribute max
+    if (this.hasAttribute('max')) {
+      max = this.getAttribute('max');
+    }
+    // disable
+    if (this.hasAttribute('disabled')) {
+      disabled = 'disabled';
+    }
+    // store the string indicating vertical orientation
+    if (this.hasAttribute('vertical')) {
+      vertical = this.getAttribute('vertical');
+    }
+    // store the value of steps
+    if (this.hasAttribute('steps')) {
+      steps = this.getAttribute('steps');
+    }
+    // store the value of 
+    if (this.hasAttribute('size')) {
+      var size_str = this.getAttribute('size');
+      if (size_str == 'L')
+        input_class = 'large-input';
+      else if(size_str == 'M')
+        input_class = 'medium-input';
+    }
 
+    // indicate whether need to show input box
+    if (this.hasAttribute('show_input')) {
+      show_input =  `<input type="number" name="amountInput" min=` + min + ` max=`
+      + max +
+      `  value=` + value + ` class=` + input_class +
+      ` oninput="this.form.amountRange.value=this.value" ` +
+      disabled + `/>`;
+    }
+
+    let input_str = importStyle +
+      `<div class="slidecontainer">`+
+          `<form>
+              <input type="range" name="amountRange" min=` + min + ` max=` + max +
+              `  value=` + value +
+              ` oninput="this.form.amountInput.value=this.value" `
+              + disabled + `/>` + show_input + `      
+          </form>
+      </div>`;
+
+    console.log(input_str);
+
+    // Set HTML
+    CustomSlider.template.innerHTML = input_str;
 
     shadowRoot.appendChild(templateContent.cloneNode(true));
   }
 
   /** Element attached on DOM */
   connectedCallback() {
-    // code
-    var slider = document.getElementById("myRange");
-    var output = document.getElementById("demo");
-    output.innerHTML = slider.value;
-    slider.oninput = function() {
-    output.innerHTML = this.value;
-    }
+
   }
 }
 
-Slider.template = document.createElement('slider_template');
-customElements.define('slider', Slider);
+CustomSlider.template = document.createElement('CustomSlider_template');
+customElements.define('custom-slider', CustomSlider);
