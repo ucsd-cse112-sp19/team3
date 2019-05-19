1038a7b5150a876f96ce249f482c12bca2b1f3f5
Gabriel Ruiz <ruiz.gabrielf@gmail.com>
2019-05-19 16:15:01 -0700

    update: add to custom button more

--- Components/CustomButton/CustomButton.js
+++ Components/CustomButton/CustomButton.js
@@ -14,16 +14,20 @@ class CustomButton extends HTMLElement {
         // default style; user styling performed by updateStyle() below
         CustomButton.template.innerHTML = `
             <style>
+                /* custom CSS variables to distinguish certain values */
                 :host {
                     --active-text-color: #000000;
                     --active-background-color: #cccccc;
+                    --active-border-color: #000000;
                     --hover-text-color: #000000;
                     --hover-background-color: #bbbbbb;
+                    --hover-border-color: #000000;
                 }
+                /* default button style */
                 button {
                     color: #bbbbbb;
                     background-color: #000000;
-                    border: solid;
+                    border: solid #bbbbbb;
                     display: block;
                     width: 80px; /* 16:9 ratio */
                     height: 45px;
@@ -32,62 +36,133 @@ class CustomButton extends HTMLElement {
                 button:hover {
                     color: var(--hover-text-color);
                     background-color: var(--hover-background-color);
+                    border-color: var(--hover-border-color);
                 }
                 button:active {
                     color: var(--active-text-color);
                     background-color: var(--active-background-color);
+                    border-color: var(--active-border-color);
                 }
             </style>
             <button><slot></slot></button>
         `
         shadowRoot.appendChild(templateContent.cloneNode(true))
-
-        // addEventListener("click", this._onClick) (google way to add event listener)
     }
 
-    /** Called when element rendered in DOM */
+    /* List of attributes supported by the component.
+     * Component listens for changes to these attributes\
+     * and handles it using attributeChangedCallback(). */
+    static get observedAttributes() {
+        return [
+            'text-color',
+            'border-color',
+            'font',
+            'width',
+            'height',
+            'active-text-color',
+            'active-background-color',
+            'active-border-color',
+            'hover-text-color',
+            'hover-background-color',
+            'hover-border-color',
+            'rounded',
+            'circle',
+            'disabled'
+        ]
+    }
+
+    /* Called when element rendered in DOM */
     connectedCallback() {
         console.log('Rendered custom-button')
-        updateStyle(this)
+        this.updateStyle()
     }
 
+    /* Called when element destroyed */
+    disconnectedCallback() {
+        console.log('custom-button removed')
+    }
+
+    /* Handles attribute changes on the fly.
+     * I suspect we don't need parameters, the way we're doing it? */
+    attributeChangedCallback(attr, oldValue, newValue) {
+        console.log('A custom-button attribute was changed')
+        // if we start supporting non-style attributes, 
+        // e.g. href, should have an if-else here.
+        this.updateStyle()
+    }
+
+    /* Getters and setters for observed attributes because WHY NOT */
+    get textColor()                { return this.getAttribute('text-color') }
+    set textColor(val)             { this.setAttribute('text-color', val) }
+    get borderColor()              { return this.getAttribute('border-color') }
+    set borderColor(val)           { this.setAttribute('border-color', val) }
+    get font()                     { return this.getAttribute('font') }
+    set font(val)                  { this.setAttribute('font', val) }
+    get width()                    { return this.getAttribute('width') }
+    set width(val)                 { this.setAttribute('width', val) }
+    get height()                   { return this.getAttribute('height') }
+    set height(val)                { this.setAttribute('height', val) }
+    get activeTextColor()          { return this.getAttribute('active-text-color') }
+    set activeTextColor(val)       { this.setAttribute('active-text-color', val) }
+    get activeBackgroundColor()    { return this.getAttribute('active-background-color') }
+    set activeBackgroundColor(val) { this.setAttribute('active-background-color', val) }
+    get activeBorderColor()        { return this.getAttribute('active-border-color') }
+    set activeBorderColor(val)     { this.setAttribute('active-border-color', val) }
+    get hoverTextColor()           { return this.getAttribute('hover-text-color') }
+    set hoverTextColor(val)        { this.setAttribute('hover-text-color', val) }
+    get hoverBackgroundColor()     { return this.getAttribute('hover-background-color') }
+    set hoverBackgroundColor(val)  { this.setAttribute('hover-background-color', val) }
+    get hoverBorderColor()         { return this.getAttribute('hover-border-color') }
+    set hoverBorderColor(val)      { this.setAttribute('hover-border-color', val) }
+    get rounded()                  { return this.getAttribute('rounded') }
+    set rounded(val)               { this.setAttribute('rounded', val) }
+    get circle()                   { return this.getAttribute('circle') }
+    set circle(val)                { this.setAttribute('circle', val) }
+    get disabled()                 { return this.getAttribute('disabled') }
+    set disabled(val)              { this.setAttribute('disabled', val) }
+
+    // Updates the style by taking user attributes, including 'style' and our own
+    // API of special attributes, and passing them to the CSS for the button.
+    // If the user styles an attribute (e.g. font color) using both direct CSS
+    // and our custom attribute (text-color), it will prioritize the custom attribute.
+    updateStyle() {
+        console.log('Updating custom-button styles')    
+
+        const shadow = this.shadowRoot
+
+        shadow.querySelector('style').textContent += `
+            button {
+                ${this.getAttribute('style')};
+                color: ${this.getAttribute('text-color')};
+                border-color: ${this.getAttribute('border-color')};
+                font-family: ${this.getAttribute('font')};
+                width: ${this.getAttribute('width')};
+                height: ${this.getAttribute('height')};
+            }
+            button:active {
+                color: ${this.getAttribute('active-text-color')};
+                background-color: ${this.getAttribute('active-background-color')};
+                border-color: ${this.getAttribute('active-border-color')};
+            }
+            button:hover {
+                color: ${this.getAttribute('hover-text-color')};
+                background-color: ${this.getAttribute('hover-background-color')};
+                border-color: ${this.getAttribute('hover-border-color')};
+            }
+            :host([rounded]) > button {
+                border-radius: 100px;
+            }
+            :host([circle]) > button {
+                width: 70px;
+                height: 70px;
+                border-radius: 100%;
+            }
+            :host([disabled]) > button {
+                cursor: not-allowed;
+            }
+        `
+    }
 }
-
-// Updates the style by taking user attributes, including 'style' and our own
-// API of special attributes, and passing them to the CSS for the button.
-// If the user styles an attribute (e.g. font color) using both direct CSS
-// and our custom attribute (text-color), it will prioritize the custom attribute.
-function updateStyle(elem) {
-    console.log('Updating custom-button styles')    
-
-    const shadow = elem.shadowRoot
-
-    shadow.querySelector('style').textContent += `
-        button {
-            ${elem.getAttribute('style')};
-            color: ${elem.getAttribute('text-color')};
-            font-family: ${elem.getAttribute('font')};
-            width: ${elem.getAttribute('width')};
-            height: ${elem.getAttribute('height')};
-        }
-        button:active {
-            color: ${elem.getAttribute('active-text-color')};
-            background-color: ${elem.getAttribute('active-background-color')};
-        }
-        button:hover {
-            color: ${elem.getAttribute('hover-text-color')};
-            background-color: ${elem.getAttribute('hover-background-color')};
-        }
-        :host([rounded]) > button {
-            border-radius: 100px;
-        }
-        :host([circle]) > button {
-            width: 70px;
-            height: 70px;
-            border-radius: 100%;
-        }
-    `
-}
-      
+     
 CustomButton.template = document.createElement('template')
 customElements.define('custom-button', CustomButton)
