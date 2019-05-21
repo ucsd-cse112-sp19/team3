8b581f4c85adfc7bc2facc65a2f9faf325b35e3d
Gabriel Ruiz <gfruiz@ucsd.edu>
2019-05-20 17:58:42 -0700

    update: continue coding popover

--- Components/CustomPopover/CustomPopover.js
+++ Components/CustomPopover/CustomPopover.js
@@ -1,4 +1,5 @@
 'use strict'
+import Popper from 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js'
 
 /** Class for Custom Popover component */
 class CustomPopover extends HTMLElement {
@@ -18,9 +19,7 @@ class CustomPopover extends HTMLElement {
             <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
             <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
             <style></style>
-            <a style='text-decoration: none'>
-            <button type='button'><slot></slot></button>
-            </a>
+            <div><slot></slot></div>
         `
         shadowRoot.appendChild(templateContent.cloneNode(true))
     }
@@ -31,29 +30,19 @@ class CustomPopover extends HTMLElement {
     static get observedAttributes() {
         return [
             'style',
+            'color',
             'text-color',
-            'background-color',
-            'border-color',
+            'anchor',
+            'placement',
             'font',
-            'width',
-            'height',
-            'hover-text-color',
-            'hover-background-color',
-            'hover-border-color',
-            'active-text-color',
-            'active-background-color',
-            'active-border-color',
-            'rounded',
-            'circle',
-            'disabled',
-            'href'
+            'header-text'
         ]
     }
 
     /* Called when element rendered in DOM */
     connectedCallback() {
         console.log('Rendered custom-popover')
-        this.updateStyle()
+        createPopper()
     }
 
     /* Called when element destroyed */
@@ -64,46 +53,46 @@ class CustomPopover extends HTMLElement {
     /* Handles attribute changes on the fly. */
     attributeChangedCallback(attr, oldValue, newValue) {
         console.log('custom-popover attribute ' + attr + ' was changed')
-        if(attr === 'href')
-            this.shadowRoot.querySelector('a').setAttribute('href', newValue)
-        else
-            this.updateStyle()
+        this.updateStyle()
     }
 
     /* Getters and setters for observed attributes because WHY NOT */
-    get style()                    { return this.getAttribute('style') }
-    set style(val)                 { this.setAttribute('style', val) }
-    get textColor()                { return this.getAttribute('text-color') }
-    set textColor(val)             { this.setAttribute('text-color', val) }
-    get backgroundColor()          { return this.getAttribute('background-color') }
-    set backgroundColor(val)       { this.setAttribute('background-color', val) }
-    get borderColor()              { return this.getAttribute('border-color') }
-    set borderColor(val)           { this.setAttribute('border-color', val) }
-    get font()                     { return this.getAttribute('font') }
-    set font(val)                  { this.setAttribute('font', val) }
-    get width()                    { return this.getAttribute('width') }
-    set width(val)                 { this.setAttribute('width', val) }
-    get height()                   { return this.getAttribute('height') }
-    set height(val)                { this.setAttribute('height', val) }
-    get hoverTextColor()           { return this.getAttribute('hover-text-color') }
-    set hoverTextColor(val)        { this.setAttribute('hover-text-color', val) }
-    get hoverBackgroundColor()     { return this.getAttribute('hover-background-color') }
-    set hoverBackgroundColor(val)  { this.setAttribute('hover-background-color', val) }
-    get hoverBorderColor()         { return this.getAttribute('hover-border-color') }
-    set hoverBorderColor(val)      { this.setAttribute('hover-border-color', val) }
-    get activeTextColor()          { return this.getAttribute('active-text-color') }
-    set activeTextColor(val)       { this.setAttribute('active-text-color', val) }
-    get activeBackgroundColor()    { return this.getAttribute('active-background-color') }
-    set activeBackgroundColor(val) { this.setAttribute('active-background-color', val) }
-    get activeBorderColor()        { return this.getAttribute('active-border-color') }
-    set activeBorderColor(val)     { this.setAttribute('active-border-color', val) }
-    get rounded()                  { return this.getAttribute('rounded') }
-    get isRounded()                { return this.hasAttribute('rounded') }
-    set rounded(val)               { this.setAttribute('rounded', val) }
-    get circle()                   { return this.getAttribute('circle') }
-    set circle(val)                { this.setAttribute('circle', val) }
-    get disabled()                 { return this.getAttribute('disabled') }
-    set disabled(val)              { this.setAttribute('disabled', val) }
+    get style()         { return this.getAttribute('style') }
+    set style(val)      { this.setAttribute('style', val) }
+    get color()         { return this.getAttribute('color') }
+    set color(val)      { this.setAttribute('color', val) }
+    get textColor()     { return this.getAttribute('text-color') }
+    set textColor(val)  { this.setAttribute('text-color', val) }
+    get anchor()        { return this.getAttribute('anchor') }
+    set anchor(val)     { this.setAttribute('anchor', val) }
+    get placement()     { return this.getAttribute('placement') }
+    set placement(val)  { this.setAttribute('placement', val) }
+    get font()     { return this.getAttribute('font') }
+    set font(val)  { this.setAttribute('font', val) }
+    get headerText()    { return this.getAttribute('header-text') }
+    set headerText(val) { this.setAttribute('header-text', val) }
+
+    // takes the anchor attribute and finds the element with that id
+    findAnchor() {
+        let anchorID = this.getAttribute('anchor')
+        if(!anchorID) {
+            console.log('ERROR: component needs an anchor attribute')
+            return null
+        }
+        const anchor = document.getElementById(anchorID)
+        if(!anchor) {
+            console.log('ERROR: cannot find element by ID: ' + anchorID)
+            return null
+        }
+        return anchor
+    }
+
+    // uses Popper.js to create a Popper object and attaches it to the anchor
+    createPopper() {
+        let anchor = findAnchor()
+
+        
+    }
 
     // Updates the style by taking user attributes, including 'style' and our own
     // API of special attributes, and passing them to the CSS for the popover.
@@ -121,46 +110,12 @@ class CustomPopover extends HTMLElement {
         // provides default styles in case the user doesn't provide one
         shadow.querySelector('style').textContent = `
             :host {
-                --hover-text-color: ${this.hoverTextColor ? this.hoverTextColor : '#000000'};
-                --hover-background-color: ${this.hoverBackgroundColor ? this.hoverBackgroundColor : '#cccccc'};
-                --hover-border-color: ${this.hoverBorderColor  ? this.hoverBorderColor : '#000000'};
-                --active-text-color: ${this.activeTextColor ? this.activeTextColor : '#000000'};
-                --active-background-color: ${this.activeBackgroundColor ? this.activeBackgroundColor : '#dddddd'};
-                --active-border-color: ${this.activeBorderColor ? this.activeBorderColor : '#000000'};
+                --color-scheme: ${this.color ? this.color : '#000000'};
+                --text-color: ${this.textColor ? this.textColor : '#000000'};
             }
-            button {
+            div {
                 ${this.style};
-                color: ${this.textColor ? this.textColor : '#cccccc'};
-                background-color: ${this.backgroundColor ? this.backgroundColor : '#000000'};
-                border: solid ${this.borderColor ? this.borderColor : '#cccccc'};
-                font-family: ${this.font ? this.font : 'Lucida Console'};
-                display: block;
-                width: ${this.width ? this.width : '96px'};
-                height: ${this.height ? this.height : '54px'};
-            }
-            button:hover {
-                color: var(--hover-text-color);
-                background-color: var(--hover-background-color);
-                border-color: var(--hover-border-color);
-            }
-            button:active {
-                color: var(--active-text-color);
-                background-color: var(--active-background-color);
-                border-color: var(--active-border-color);
-            }
-            :host([rounded]) > button {
-                border-radius: 100px;
-            }
-            :host([circle]) > button {
-                width: 70px;
-                height: 70px;
-                border-radius: 100%;
-            }
-            :host([disabled]) > button {
-                cursor: not-allowed;
-            }
-            :host > button:focus {
-                outline: none;
+                font-family: ${this.font ? this.font : 'Trebuchet MS'};
             }
         `
     }
