6436fc1304bb343022a57c38e2bef931e10ec6f0
Gabriel Ruiz <ruiz.gabrielf@gmail.com>
2019-06-01 20:59:01 -0700

    more work on popover

--- Components/CustomPopover/CustomPopover.js
+++ Components/CustomPopover/CustomPopover.js
@@ -1,5 +1,4 @@
 'use strict'
-import Popper from 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js'
 
 /** Class for Custom Popover component */
 class CustomPopover extends HTMLElement {
@@ -39,23 +38,6 @@ class CustomPopover extends HTMLElement {
         ]
     }
 
-    /* Called when element rendered in DOM */
-    connectedCallback() {
-        console.log('Rendered custom-popover')
-        createPopper()
-    }
-
-    /* Called when element destroyed */
-    disconnectedCallback() {
-        console.log('custom-popover removed')
-    }
-
-    /* Handles attribute changes on the fly. */
-    attributeChangedCallback(attr, oldValue, newValue) {
-        console.log('custom-popover attribute ' + attr + ' was changed')
-        this.updateStyle()
-    }
-
     /* Getters and setters for observed attributes because WHY NOT */
     get style()         { return this.getAttribute('style') }
     set style(val)      { this.setAttribute('style', val) }
@@ -67,13 +49,14 @@ class CustomPopover extends HTMLElement {
     set anchor(val)     { this.setAttribute('anchor', val) }
     get placement()     { return this.getAttribute('placement') }
     set placement(val)  { this.setAttribute('placement', val) }
-    get font()     { return this.getAttribute('font') }
-    set font(val)  { this.setAttribute('font', val) }
+    get font()          { return this.getAttribute('font') }
+    set font(val)       { this.setAttribute('font', val) }
     get headerText()    { return this.getAttribute('header-text') }
     set headerText(val) { this.setAttribute('header-text', val) }
 
     // takes the anchor attribute and finds the element with that id
     findAnchor() {
+        console.log('running findAnchor()')
         let anchorID = this.getAttribute('anchor')
         if(!anchorID) {
             console.log('ERROR: component needs an anchor attribute')
@@ -87,11 +70,48 @@ class CustomPopover extends HTMLElement {
         return anchor
     }
 
-    // uses Popper.js to create a Popper object and attaches it to the anchor
-    createPopper() {
-        let anchor = findAnchor()
+    // calls findAnchor() and attaches it to the popover as well as 
+    // setting up properties the anchor should have such as mouseover properties
+    attachAnchor() {
+        console.log('running attachAnchor()')
+        let anchor = this.findAnchor()
 
-        
+        // this gets injected in the style of the anchor element
+        var anchorCSS = '; position: relative; display: inline-block;'
+        anchor.setAttribute('style', anchor.getAttribute('style') + anchorCSS)
+
+        // this is hacky as hell but it works
+        let shadow = this.shadowRoot
+        anchor.onmouseover = function() {
+            shadow.querySelector('style').textContent += 'div {visibility: visible;}'
+        }
+        anchor.onmouseout = function() {
+            shadow.querySelector('style').textContent += 'div {visibility: hidden;}'
+        }
+
+        this.updateStyle()
+    }
+
+    /* Called when element rendered in DOM */
+    connectedCallback() {
+        console.log('Rendering custom-popover')
+        this.attachAnchor()
+        this.updateStyle()
+        console.log('Rendering custom-popover complete')
+    }
+
+    /* Called when element destroyed */
+    disconnectedCallback() {
+        console.log('custom-popover removed')
+    }
+
+    /* Handles attribute changes on the fly. */
+    attributeChangedCallback(attr, oldValue, newValue) {
+        console.log('custom-popover attribute ' + attr + ' was changed')
+        if (attr === 'anchor')
+            this.attachAnchor()
+        else
+            this.updateStyle()
     }
 
     // Updates the style by taking user attributes, including 'style' and our own
@@ -104,19 +124,46 @@ class CustomPopover extends HTMLElement {
         const shadow = this.shadowRoot
 
         // pass class attribute into the component
-        const btn = shadow.querySelector('button')
-        btn.setAttribute('class', this.getAttribute('class'))
+        const div = shadow.querySelector('div')
+        div.setAttribute('class', this.getAttribute('class'))
 
         // provides default styles in case the user doesn't provide one
         shadow.querySelector('style').textContent = `
             :host {
-                --color-scheme: ${this.color ? this.color : '#000000'};
-                --text-color: ${this.textColor ? this.textColor : '#000000'};
+                --color-scheme: ${this.color ? this.color : '#45b9e8'};
+                --text-color: ${this.textColor ? this.textColor : '#ffffff'};
             }
             div {
-                ${this.style};
-                font-family: ${this.font ? this.font : 'Trebuchet MS'};
+                background-color: var(--color-scheme);
+                color: var(--text-color);
+                font-family: ${this.font ? this.font : 'Lucida Console'};    
+                position: relative;
+                display: inline-block;
+
+                visibility: hidden;
+                width: 120px;
+
+                text-align: center;
+                border: solid 1px;
+                border-radius: 6px;
+                padding: 6px 6px;
+
+                position: absolute;
+                z-index: 1;
             }
+        `
+        switch (this.placement) {
+            case 'top':
+                break
+            case 'bottom':
+                break
+            case 'left':
+                break
+            case 'right':
+                break
+            default:
+        }
+        shadow.querySelector('style').textContent += `
         `
     }
 }
