c9f6df717fdb74717138ab3bbba53a439a140863
Vince Li <vince1114@gmail.com>
2019-06-04 21:42:32 -0700

    test: additional button tests (#40)
    * test: updated the button test
    
    * test: successful to test the rouned shape
    
    * done testing the circle shape
    
    * test: button test done other than href (not sure)
    
    * test: added theme and disabled test

--- Tests/Button.js
+++ Tests/Button.js
@@ -38,7 +38,13 @@ describe('CustomButton', function() {
     it('Testing circle shape of the button', async() => {
       await showroom.setAttribute('circle', 1)
       const shape = await showroom.getAttribute('circle')
-      assert.deepEqual(shape, '1')
+      const style = await showroom.find('//style', component)
+      var style_text_content = await showroom.getTextContent(style)
+      var width = style_text_content.includes("width: 70px;")
+      var height = style_text_content.includes("height: 70px;")
+      var border_radius = style_text_content.includes("border-radius: 100%;")
+      var result = (border_radius & width & height).toString()
+      assert.deepEqual(result, '1')
     });
   });
 
@@ -82,5 +88,85 @@ describe('CustomButton', function() {
     });
 
   });
+
+  describe('Theme', async() => {
+    it('Testing the ocean theme', async() => {
+      await showroom.setAttribute('theme', 'ocean')
+      const style = await showroom.find('//style', component)
+      const text = await showroom.getTextContent(style)
+      const result = text.includes("color: #7fffd4;")
+      assert.deepEqual(result.toString(), 'true')
+    });
+
+    it('Testing the mad-queen theme', async() => {
+      await showroom.setAttribute('theme', 'mad-queen')
+      const style = await showroom.find('//style', component)
+      const text = await showroom.getTextContent(style)
+      const result = text.includes("color: #dd0000;")
+      assert.deepEqual(result.toString(), 'true')
+    });
+
+    it('Testing the desert theme', async() => {
+      await showroom.setAttribute('theme', 'desert')
+      const style = await showroom.find('//style', component)
+      const text = await showroom.getTextContent(style)
+      const result = text.includes("color: #a52a2a;")
+      assert.deepEqual(result.toString(), 'true')
+    });
+
+    it('Testing the pink-lemonade theme', async() => {
+      await showroom.setAttribute('theme', 'pink-lemonade')
+      const style = await showroom.find('//style', component)
+      const text = await showroom.getTextContent(style)
+      const result = text.includes("color: #ff1493;")
+      assert.deepEqual(result.toString(), 'true')
+    });
+
+    it('Testing the forest theme', async() => {
+      await showroom.setAttribute('theme', 'forest')
+      const style = await showroom.find('//style', component)
+      const text = await showroom.getTextContent(style)
+      const result = text.includes("color: #52ed32;")
+      assert.deepEqual(result.toString(), 'true')
+    });
+
+    it('Testing the ghost theme', async() => {
+      await showroom.setAttribute('theme', 'ghost')
+      const style = await showroom.find('//style', component)
+      const text = await showroom.getTextContent(style)
+      const result = text.includes("color: #aaaaaa;")
+      assert.deepEqual(result.toString(), 'true')
+    });
+
+    it('Testing the flame theme', async() => {
+      await showroom.setAttribute('theme', 'flame')
+      const style = await showroom.find('//style', component)
+      const text = await showroom.getTextContent(style)
+      const result = text.includes("color: #ffbb00;")
+      assert.deepEqual(result.toString(), 'true')
+    });
+
+    it('Testing the triton theme', async() => {
+      await showroom.setAttribute('theme', 'triton')
+      const style = await showroom.find('//style', component)
+      const text = await showroom.getTextContent(style)
+      const result = text.includes("color: #ffd700;")
+      assert.deepEqual(result.toString(), 'true')
+    });
+
+  });
+
+  describe('disabled functionality', async() => {
+    it('Testing the disabled', async() => {
+      await showroom.setAttribute('disabled', '')
+      const style = await showroom.find('//style', component)
+      const text = await showroom.getTextContent(style)
+      const result = text.includes("cursor: not-allowed;")
+      assert.deepEqual(result.toString(), 'true')
+    });
+
+  });
+
+  
 
 });
