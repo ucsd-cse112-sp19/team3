5b5f651aa69f4a8b58f7b852672ce6c713ad40be
rixlai <rixktl@gmail.com>
2019-06-07 14:12:59 -0700

    fix: save all coverages until write

--- Tests/tests.js
+++ Tests/tests.js
@@ -1,5 +1,17 @@
 'use strict';
 
-require('./CoreHello');
-require('./Button');
-require('./Popover');
+const coverage = require('../Utils/coverage.js');
+
+describe('All Tests', function() {
+  before(function() {
+    coverage.initCoverage();
+  });
+
+  require('./CoreHello');
+  require('./Button');
+  require('./Popover');
+
+  after(function() {
+    coverage.writeOutCoverage();
+  });
+});
