0cdf68da112ec3e8d16372073a9e5f0a33918404
Alex Z <alexz1012@yahoo.com>
2019-06-10 16:52:40 -0700

    Add more tests to slider

--- Tests/Slider.js
+++ Tests/Slider.js
@@ -26,15 +26,38 @@ describe('CustomSlider', function() {
   });
 
   describe('Testing the attributes of the slider', async () => {
-    it('Testing min attributes', async () => {
+    it('Testing min attribute', async () => {
       await showroom.setAttribute('min', 25);
       const find = await showroom.find('//input', component);
       const minprop = await find.getProperty('min');
       const min = minprop._remoteObject.value;
       assert.deepEqual(min, '25');
-
-
-      // assert.deepEqual(result.toString(), 'true');
+    });
+    it('Testing max attribute', async () => {
+      await showroom.setAttribute('max', 200);
+      const find = await showroom.find('//input', component);
+      const maxprop = await find.getProperty('max');
+      const max = maxprop._remoteObject.value;
+      assert.deepEqual(max, '200');
+    });
+    it('Testing step attribute', async () => {
+      await showroom.setAttribute('step', 5);
+      const find = await showroom.find('//input', component);
+      const stepprop = await find.getProperty('step');
+      const step = stepprop._remoteObject.value;
+      assert.deepEqual(step, '5');
+    });
+    it('Testing value attribute', async () => {
+      await showroom.setAttribute('value', 5);
+      const find = await showroom.find('//input', component);
+      const valueprop = await find.getProperty('value');
+      const value = valueprop._remoteObject.value;
+      assert.deepEqual(value, '5');
+    });
+    it('Testing disable attribute', async () => {
+      await showroom.setAttribute('slider-class', 'testClass');
+      const obj = await showroom.find('//input', component);
+      assert.deepEqual(obj._remoteObject.description, 'input.testClass');
     });
   });
 });
