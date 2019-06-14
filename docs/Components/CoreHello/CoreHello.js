1672ae64f1b1fffc59be9d99d2ce08649044a61e
Brayden Crosta <crosta.brayden@gmail.com>
2019-06-13 21:50:13 -0700

    fix: fix docs folders (#69)
    * chore: make doc components work again
    
    * chore: fix readme

---
+++ docs/Components/CoreHello/CoreHello.js
@@ -0,0 +1,194 @@
+'use strict';
+
+/**
+ * @file CoreHello
+ */
+class CoreHello extends HTMLElement {
+  /**
+   * @class CoreHello
+   * @classdesc A starter of customized web components.
+   * @extends HTMLElement
+   * @summary
+   * ### Usage
+   * *See [CoreHello Demo]{@link https://ucsd-cse112.github.io/team3/demo_components/CoreHello/CoreHello.html} for all listing examples.*
+   * ```
+   * <core-hello property="value">
+   *     Display Text
+   * </core-hello>
+   * ```
+   * @property {string} style - Same as style attribute in native CSS.
+   * @property {string} class - Same as class attribute in native CSS.
+   * @property {string} lang - Set the language of display, default is English,
+   * other options: jp, es, fr
+   * @property {boolean} rainbow - Enable rainbow color effect.
+   *
+   * @example
+   * <core-hello>
+   *     Peter
+   * </core-hello>
+   *
+   * @example
+   * <core-hello lang="es" rainbow>
+   *     Peter
+   * </core-hello>
+   *
+   * @see [CoreHello Demo]{@link https://ucsd-cse112.github.io/team3/demo_components/CoreHello/CoreHello.html} for all listing examples.
+   */
+  constructor() {
+    super();
+
+    // Create shadow DOM and attach template content
+    const shadowRoot = this.attachShadow({mode: 'open'});
+    CoreHello.template = document.createElement('template');
+    const templateContent = CoreHello.template.content;
+
+    // component skeleton - fleshed out by callback functions
+    // and updateStyle()
+    CoreHello.template.innerHTML = `
+      <style>
+      </style>
+      <div><greeting>Hello World </greeting><slot></slot></div>`;
+
+    shadowRoot.appendChild(templateContent.cloneNode(true));
+  }
+
+  /**
+   * List of attributes supported by the component.
+   * Component listens for changes to these attributes
+   * and handles it using attributeChangedCallback().
+   */
+  static get observedAttributes() {
+    return [
+      'style',
+      'class',
+      'lang',
+      'rainbow',
+    ];
+  }
+
+  /** Called when element rendered in DOM */
+  connectedCallback() {
+    console.log('core-hello rendered in DOM');
+  }
+
+  /** Called when element destroyed */
+  disconnectedCallback() {
+    console.log('core-hello removed from DOM');
+  }
+
+  /**
+    * Handles attribute changes on the fly.
+    * @param {string} attr - The attribute changed
+    * @param {string} oldValue - The value before change
+    * @param {string} newValue - The value after change
+    */
+  attributeChangedCallback(attr, oldValue, newValue) {
+    console.log('core-hello attribute ' + attr + ' changed');
+    if (attr === 'lang') {
+      // Set greeting
+      let greeting = '';
+      switch (newValue) {
+        case 'jp':
+          greeting = 'Kon\'nichiwa sekai';
+          break;
+        case 'es':
+          greeting = 'Hola Mundo';
+          break;
+        case 'fr':
+          greeting = 'Bonjour le monde';
+          break;
+        default:
+          greeting = 'Hello World';
+      }
+      const shadow = this.shadowRoot;
+      shadow.querySelector('greeting').textContent = greeting + ' ';
+    } else {
+      this.updateStyle();
+    }
+  }
+
+  /** Updates style based on set attributes */
+  updateStyle() {
+    console.log('core-hello calling updateStyle()');
+    const shadow = this.shadowRoot;
+
+    const div = shadow.querySelector('div');
+    if (this.hasAttribute('class')) {
+      div.setAttribute('class', this.getAttribute('class'));
+    }
+    if (this.hasAttribute('style')) {
+      div.setAttribute('style', this.getAttribute('style'));
+    }
+    shadow.querySelector('style').textContent = `
+      div { /* greeting styles, always included */
+        font-size: 100px;
+        font-family: 'Franklin Gothic Medium',
+        'Arial Narrow', Arial, sans-serif;
+      }
+      .rainbow {
+        animation: rainbow 2.5s linear;
+        animation-iteration-count: infinite;
+      }
+
+      @keyframes rainbow {
+        100%,
+        0% {
+      color: rgb(255, 0, 0);
+        }
+
+        8% {
+      color: rgb(255, 127, 0);
+        }
+
+        16% {
+      color: rgb(255, 255, 0);
+        }
+
+        25% {
+      color: rgb(127, 255, 0);
+        }
+
+        33% {
+      color: rgb(0, 255, 0);
+        }
+
+        41% {
+      color: rgb(0, 255, 127);
+        }
+
+        50% {
+      color: rgb(0, 255, 255);
+        }
+
+        58% {
+      color: rgb(0, 127, 255);
+        }
+
+        66% {
+      color: rgb(0, 0, 255);
+        }
+
+        75% {
+      color: rgb(127, 0, 255);
+        }
+
+        83% {
+      color: rgb(255, 0, 255);
+        }
+
+        91% {
+      color: rgb(255, 0, 127);
+        }
+      }
+    `;
+
+    // Set rainbow style
+    if (this.hasAttribute('rainbow')) {
+      div.setAttribute('class', 'rainbow');
+    }
+  }
+}
+
+/** */
+CoreHello.template = document.createElement('template');
+customElements.define('core-hello', CoreHello);
---
+++ docs/Components/CustomSlider/CustomSlider.js
@@ -0,0 +1,271 @@
+'use strict';
+
+/**
+ * @file CustomSlider
+ */
+class CustomSlider extends HTMLElement {
+  /**
+   * @class CustomSlider
+   * @classdesc Create a new CustomSlider component with designated styles.
+   * @summary
+   * ### Usage
+   * *See [CustomSlider Demo]{@link https://ucsd-cse112.github.io/team3/demo_components/CustomSlider/CustomSlider.html} for all listing examples.*
+   * ```
+   * <custom-slider property="value"></custom-slider>
+   * ```
+   * @property {string} min - Set the minimum value of the slider.
+   * @property {string} max - Set the maximum value of the slider.
+   * @property {string} step - Set the step size when moving the slider.
+   * @property {string} value - Set the initial value of the slider.
+   * @property {boolean} disabled - Make the slider unabled to be used.
+   * @property {boolean} readonly - Set the input field as read-only.
+   * @property {boolean} required - Set whether required for like a form (if
+   * there’s a form bounding this component).
+   * @property {boolean} showinput - Enables a text box containing the current
+   * value of the slider.
+   * @property {string} onchange - Callback function when value is updated.
+   * @property {string} slider-class - Class attribute for the slider in native
+   * CSS.
+   * @property {String} input-class - Class attribute for the input field in
+   * native CSS.
+   *
+   * @example
+   * <custom-slider showinput></custom-slider>
+   *
+   * @example
+   * <custom-slider min="0" max="10" value="5" showinput>
+   * </custom-slider>
+   *
+   * @example
+   * <custom-slider min="0" max="10" value="5" showinput disabled>
+   * </custom-slider>
+   *
+   * @example
+   * <custom-slider min="0" max="10" value="5" showinput onchange="changed">
+   * </custom-slider>
+   *
+   * @example
+   * <custom-slider showinput
+   *                input-class="form-control"
+   *                slider-class="form-control-range">
+   * </custom-slider>
+   *
+   * @see See [CustomSlider Demo]{@link https://ucsd-cse112.github.io/team3/demo_components/CustomSlider/CustomSlider.html} for all listing examples.
+   */
+  constructor() {
+    super();
+
+    // Create shadow DOM and attach template content
+    const shadowRoot = this.attachShadow({mode: 'open'});
+    CustomSlider.template = document.createElement('template');
+    const templateContent = CustomSlider.template.content;
+
+    const defaultStyle = `
+    <style>
+    input[type="range"] {
+      -webkit-appearance: none;
+      width: 500px;
+      height: 15px;
+      border-radius: 5px;
+      background: #d3d3d3;
+      outline: none;
+      opacity: 0.7;
+    }
+    
+    input[type=range]::-moz-focus-outer {
+      border: none;
+    }
+    
+    input[type="range"]:not([disabled])::-moz-range-thumb:hover {
+      height: 25px;
+      width: 25px;
+    }
+    
+    input[type="range"]::-moz-range-thumb {
+      -webkit-appearance: none;
+      height: 20px;
+      width: 20px;
+      border-radius: 50%;
+      border: 1px solid rgb(112, 120, 126);
+      background: #fff;
+      -webkit-transition: width 0.1s, height 0.1s ease-in-out;
+      transition: width 0.1s, height 0.1s ease-in-out;
+    }
+    
+    input[type="range"]:not([disabled])::-webkit-slider-thumb:hover {
+      height: 25px;
+      width: 25px;
+    }
+    
+    input[type="range"]::-webkit-slider-thumb {
+      -webkit-appearance: none;
+      height: 20px;
+      width: 20px;
+      border-radius: 50%;
+      border: 1px solid rgb(112, 120, 126);
+      background: #fff;
+      -webkit-transition: width 0.1s, height 0.1s ease-in-out;
+      transition: width 0.1s, height 0.1s ease-in-out;
+    }
+    </style>
+    `;
+
+    CustomSlider.template.innerHTML = `
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+      <input type="range" />
+      <div id="valueDisplay"/>
+    ` + defaultStyle;
+
+    shadowRoot.appendChild(templateContent.cloneNode(true));
+    this.onValueChange = this.onValueChange.bind(this);
+    shadowRoot.querySelector('input').addEventListener('input',
+        this.onValueChange);
+  }
+
+  /**
+   * Default attributes values
+   */
+  static get defaultAttributes() {
+    return {
+      'value': 0,
+      'min': 0,
+      'max': 100,
+      'showinput': true,
+    };
+  }
+
+  /**
+   * Attributes passed directly to input (range)
+   */
+  static get inputAttributes() {
+    return [
+      'min',
+      'max',
+      'step',
+      'value',
+    ];
+  }
+
+  /**
+   * Attribte tags passed directly to input (range)
+   */
+  static get tagAttributes() {
+    return [
+      'disabled',
+      'readonly',
+      'required',
+    ];
+  }
+
+  /**
+   * Custom attributes for slider
+   */
+  static get customAttributes() {
+    return [
+      'slider-class',
+      'input-class',
+      'showinput',
+      'onchange',
+    ];
+  }
+
+  /**
+   * List of attributes supported by the component.
+   * Component listens for changes to these attributes
+   * and handles it using attributeChangedCallback().
+   */
+  static get observedAttributes() {
+    // All attributes
+    return CustomSlider.inputAttributes
+        .concat(CustomSlider.customAttributes)
+        .concat(CustomSlider.tagAttributes);
+  }
+
+  /**
+   * Update style from observed attributes
+   */
+  updateStyle() {
+    const slider = this.shadowRoot.querySelector('input');
+    const valueDisplay = this.shadowRoot.querySelector('#valueDisplay');
+    // Update all attributes for input
+    for (const attribute of CustomSlider.inputAttributes) {
+      if (this.hasAttribute(attribute)) {
+        slider[attribute] = this.getAttribute(attribute);
+      } else {
+        if (CustomSlider.defaultAttributes[attribute] !== undefined) {
+          // Use default attribute if possible
+          slider[attribute] = CustomSlider.defaultAttributes[attribute];
+        }
+      }
+    }
+
+    for (const attribute of CustomSlider.tagAttributes) {
+      if (this.hasAttribute(attribute)) {
+        slider.setAttribute(attribute, true);
+      }
+    }
+
+    // showinput option tag
+    if (this.hasAttribute('showinput')) {
+      valueDisplay.style.display = '';
+    } else {
+      valueDisplay.style.display = 'none';
+    }
+
+    // Update value
+    valueDisplay.innerHTML = slider.value;
+
+    // Pass class attribute into the component
+    if (this.hasAttribute('slider-class')) {
+      slider.setAttribute('class', this.getAttribute('slider-class'));
+    }
+    if (this.hasAttribute('input-class')) {
+      valueDisplay.setAttribute('class', this.getAttribute('input-class'));
+    }
+
+    this.onValueChange(slider.value);
+  }
+
+  /**
+   * When value of the slider changed
+   * @param {Integer} value
+   */
+  onValueChange(value) {
+    const shadow = this.shadowRoot;
+    const slider = shadow.querySelector('input');
+    const valueDisplay = shadow.querySelector('#valueDisplay');
+    // Update display value
+    valueDisplay.innerHTML = slider.value;
+    // Notify onchange
+    if (this.hasAttribute('onchange')) {
+      const callbackName = this.getAttribute('onchange');
+      const cb = window[callbackName];
+      if (typeof cb === 'function') {
+        cb(slider.value);
+      }
+    }
+  }
+
+  /**
+   * Component attached on DOM
+   */
+  connectedCallback() {
+    this.updateStyle();
+  }
+
+  /**
+   * Called on attribute change
+   * @param {string} attr the attribute changed
+   * @param {string} oldValue the old value of the attribute
+   * @param {string} newValue the new value of the attribute
+   */
+  attributeChangedCallback(attr, oldValue, newValue) {
+    this.updateStyle();
+  }
+}
+
+CustomSlider.template = document.createElement('template');
+customElements.define('custom-slider', CustomSlider);
